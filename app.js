8fee8c83a0f70181241bbd28c6debc139fd8af00
Sara Tabarez <20253ds045@utez.edu.mx>
2026-06-08 08:28:17 -0600

    08/junio/2026 ultima clase

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 //Declaraciones de variables
 
- const estudiantes = [
-  { id: 1, nombre: "Ana Lopez",    nota: 90 },
-  { id: 2, nombre: "Carlos Ruiz",  nota: 55 },
-  { id: 3, nombre: "Maria Torres", nota: 78 },
-  { id: 4, nombre: "Luis Mendez",  nota: 45 },
-  { id: 5, nombre: "Sofia Rios",   nota: 88 },
-  { id: 6, nombre: "Pedro Soto",   nota: 62 },
+const estudiantes = [
+    { id: 1, nombre: "Ana Lopez", nota: 90 },
+    { id: 2, nombre: "Carlos Ruiz", nota: 55 },
+    { id: 3, nombre: "Maria Torres", nota: 78 },
+    { id: 4, nombre: "Luis Mendez", nota: 45 },
+    { id: 5, nombre: "Sofia Rios", nota: 88 },
+    { id: 6, nombre: "Pedro Soto", nota: 62 },
 ];
 
 //Un estudiante solito
 const estudianteX = {
-    id : 7,
-    nombre : "Lucia Gomez",
-    nota : 95
+    id: 7,
+    nombre: "Lucia Gomez",
+    nota: 95
 }
 
 
@@ -24,6 +24,12 @@ const btnTodos = document.getElementById("btn-todos");
 const btnAprobados = document.getElementById("btn-aprobados");
 const btnReprobados = document.getElementById("btn-reprobados");
 const btnPromedio = document.getElementById("btn-promedio");
+const seccionPromedio = document.getElementById("resultado-promedio");
+const imputNombre = document.getElementById("input-nombre");
+const imputNota = document.getElementById("input-nota");
+const btnAgregar = document.getElementById("btn-agregar");
+
+
 
 //Funciones
 
@@ -44,13 +50,20 @@ const crearTarjeta = (unEstudiante) => {
 const renderizarLista = (estudiantesApintar) => {
     const listaTarjetas = estudiantesApintar.map(
         (unEstudiante) => {
-        const tarjeta = crearTarjeta(unEstudiante);
-        return tarjeta;
+            const tarjeta = crearTarjeta(unEstudiante);
+            return tarjeta;
         }
     );
     seccionEstudiantes.innerHTML = listaTarjetas.join("");
 };
 
+const toFixedTrunc = (num, decimales) => {
+    const factor = Math.pow(10, decimales);
+
+    const truncado = Math.trunc(num * factor) / factor;
+
+    return truncado.toFixed(decimales);
+}
 
 //Eventos
 
@@ -64,7 +77,7 @@ btnAprobados.addEventListener('click',
     () => {
         const aprobados = estudiantes.filter(
             (unEstudianteX) => {
-             return unEstudianteX.nota > 60;    
+                return unEstudianteX.nota > 60;
             }
         );
         renderizarLista(aprobados);
@@ -76,12 +89,50 @@ btnReprobados.addEventListener(
     () => {
         const reprobados = estudiantes.filter(
             (unEstudianteX) => {
-             return unEstudianteX.nota < 61;    
+                return unEstudianteX.nota < 61;
             }
         );
         renderizarLista(reprobados);
     }
 );
+
+btnPromedio.addEventListener('click', () => {
+
+    const sumaNotas = estudiantes.reduce(
+        (valorPersistente, estudiante) => {
+            return valorPersistente + estudiante.nota;
+        },
+        0);
+
+    const promedio = sumaNotas / estudiantes.length;
+    console.log(toFixedTrunc(promedio, 2));
+    seccionPromedio.innerHTML = "Promedio: " + toFixedTrunc(promedio, 2);
+    seccionPromedio.style.display = "block";
+});
+
+btnAgregar.addEventListener("click", () => {
+    const nombre = imputNombre.value.trim();
+    const nota = parseInt(imputNota.value.trim());
+
+    if (nombre == "" || isNaN(nota) || nota <0 || nota > 100){
+        alert("Por favor, ingrese un nombre valido y una nota entre 0 y 100");
+        return;
+    }
+
+
+    const nuevoEstudiante = {
+        id: estudiantes.length + 1,
+        nombre: nombre,
+        nota: nota
+    };
+
+    estudiantes.push(nuevoEstudiante);
+    renderizarLista(estudiantes);
+
+    imputNombre.value = "";
+    imputNota.value = "";
+});
+
 
 //Llamadas a funciones
 
